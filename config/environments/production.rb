616a4076b2bb51eb890d89e494c3ad862c1492c8
Carlos Aguirre <c.aguirre017@gmail.com>
2016-07-18 19:26:19 -0400

    Change Host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   config.log_level = :debug
 
 
-  config.action_mailer.default_url_options = { host: 'avisotallerempleabilidad.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'tallerdeempleabilidad.herokuapp.com/' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
